b3e108c66fba98547d8fbc640ab4fbbdcb8b1379
yaelajami2 <y0556722091@gmail.com>
2025-02-04 12:04:20 +0200

    connectionString

--- obj/Debug/net8.0/yael_project.AssemblyInfo.cs
+++ obj/Debug/net8.0/yael_project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yael_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c76282b789c6c9c14280290203af07c6f1993e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+267cc45e0f3c56fdd3b349b22ad47959ad94d4ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("yael_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yael_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
